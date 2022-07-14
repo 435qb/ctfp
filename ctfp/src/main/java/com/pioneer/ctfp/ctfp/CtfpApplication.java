3861f7f3f9304eda496d0c449c4b463969578a79
qb <2920077696@qq.com>
2022-07-14 14:27:19 +0800

    initial commit

---
+++ ctfp/src/main/java/com/pioneer/ctfp/ctfp/CtfpApplication.java
@@ -0,0 +1,13 @@
+package com.pioneer.ctfp.ctfp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtfpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtfpApplication.class, args);
+	}
+
+}
---
+++ ctfp/src/test/java/com/pioneer/ctfp/ctfp/CtfpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pioneer.ctfp.ctfp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtfpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
